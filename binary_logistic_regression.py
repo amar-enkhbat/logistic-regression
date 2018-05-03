0a2732911f97d57c1c36705fd5195d17e37abd21
Amar Enkhbat <38126496+amar-enkhbat@users.noreply.github.com>
2018-05-03 20:22:08 +0900

    Added regularization
    Added regularization. Uses 2 Iris features instead of 6 points on a plane.

--- binary_logistic_regression.py
+++ binary_logistic_regression.py
@@ -8,13 +8,20 @@ Created on Tue Apr 10 17:36:39 2018
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+from sklearn import datasets
 # =============================================================================
 # Importing data
 # =============================================================================
 
 X = np.array([[1, 1], [2, 1], [1, 5], [6, 4], [5, 5], [6, 5]], dtype = float)
 y = np.array([0, 0, 1, 1, 1, 1])
+
+iris = datasets.load_iris()
+X = iris.data[:, 0:2]
+y = iris.target
+
+X = X[y != 2]
+y = y[y != 2]
 
 
 # =============================================================================
@@ -62,7 +69,9 @@ def activate(x):
 plt.scatter(X_std[y == 0, 0], X_std[y == 0, 1], label = "Class 0")
 plt.scatter(X_std[y == 1, 0], X_std[y == 1, 1], label = "Class 1")
 t = np.arange(-2, 2, 0.1)
-plt.plot(t, (-weight[0] - weight[1] * t)/weight[2])
+plt.plot(t, (-weight[0] - weight[1] * t)/weight[2], label = "Decision boundary")
+plt.xlabel("Feature 1")
+plt.ylabel("Feature 2")
 plt.legend()
 plt.show()
 
@@ -71,27 +80,51 @@ plt.show()
 # =============================================================================
 
 # Learning Rate
-eta = 0.1
+eta = 0.01
 
 # Number of iterations
-epoch = 20
+epoch = 10000
 
+# Regularization coef
+cost_lambda = 0.1
+
+# Cost array
+cost_series = []
 
 # Fitting/Training
 for i in range(epoch):
     # Dot product of z (= w1*x1 + w2*x2 + ... + w6*x6) + w0
     net_input = np.dot(X_std, weight[1:]) + weight[0]
     # Cost function (Cross Entropy Function)
-    cost = -(y * (np.log(activate(net_input))) + (1 - y) * np.log(1 - activate(net_input)))
+    cost = ((y.dot((np.log(activate(net_input)))) + (1 - y).dot(np.log(1 - activate(net_input)))) + cost_lambda * (weight[1:]**2).sum()).sum() / (-X.shape[1])
+    cost_series.append(cost)
     
     # Weight update
     weight[1:] += eta * np.dot((y - activate(net_input)), X_std)
     weight[0] += eta * np.sum(y - activate(net_input))
     
+    if(i > 2):
+        if (cost_series[-2] - cost_series[-1]) < 0.001:
+            break
     # Plot decision boundary
-    plt.scatter(X_std[y == 0, 0], X_std[y == 0, 1], label = "Class 0")
-    plt.scatter(X_std[y == 1, 0], X_std[y == 1, 1], label = "Class 1")
-    t = np.arange(-2, 2, 0.1)
-    plt.plot(t, (-weight[0] - weight[1] * t)/weight[2])
-    plt.legend()
-    plt.show()
+#    plt.scatter(X_std[y == 0, 0], X_std[y == 0, 1], label = "Class 0")
+#    plt.scatter(X_std[y == 1, 0], X_std[y == 1, 1], label = "Class 1")
+#    t = np.arange(-2, 2, 0.1)
+#    plt.plot(t, (-weight[0] - weight[1] * t)/weight[2])
+#    plt.legend()
+#    plt.show()
+    
+cost = np.array(cost)
+plt.plot(range(len(cost_series)), cost_series, label = "Cost")
+plt.xlabel("Epoch")
+plt.ylabel("Cost")
+plt.show()
+plt.scatter(X_std[y == 0, 0], X_std[y == 0, 1], label = "Class 0")
+plt.scatter(X_std[y == 1, 0], X_std[y == 1, 1], label = "Class 1")
+plt.xlabel("Feature 1")
+plt.ylabel("Feature 2")
+t = np.arange(-2, 2, 0.1)
+plt.plot(t, (-weight[0] - weight[1] * t)/weight[2], label = "Decision boundary")
+plt.legend()
+plt.show()
+print("Suggested epoch number:", len(cost_series))
